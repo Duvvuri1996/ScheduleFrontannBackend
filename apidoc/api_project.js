e8d51b981bf33f7e1003626c7a0508f7a4bb949c
Sowmya <dsssri17@gmail.com>
2020-08-13 16:47:46 +0530

    second commit

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Meeting Organizer",
+  "version": "1.0.0",
+  "description": "Documentation for the Meeting Organizer",
+  "title": "Meeting Organizer APIs",
+  "url": "http://localhost:3000",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-07-07T07:48:04.996Z",
+    "url": "http://apidocjs.com",
+    "version": "0.23.0"
+  }
+});
